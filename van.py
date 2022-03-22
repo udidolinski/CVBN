8ac8ca1428497f9371440765df459ef948f06b52
noamoa <noa.moalem@mail.huji.ac.il>
2022-03-22 17:55:57 +0200

    Done ex1

--- van.py
+++ van.py
@@ -18,25 +18,19 @@ def show_key_points(idx, kps1, kps2):
     img1, img2 = read_images(idx, RGB)
     cv2.drawKeypoints(img1, kps1[:500], img1, (255, 0, 0),
                       flags=cv2.DRAW_MATCHES_FLAGS_DRAW_OVER_OUTIMG)
-    # cv2.imwrite("output1.png", img1)
     cv2.imshow("Output Image 1", img1)
     cv2.drawKeypoints(img1, kps2[:500], img2, (255, 0, 0),
                       flags=cv2.DRAW_MATCHES_FLAGS_DRAW_OVER_OUTIMG)
-    # cv2.imwrite("output2.png", img2)
     cv2.imshow("Output Image 2", img1)
     cv2.waitKey(0)
 
 
 def detect_key_points(idx):
     img1, img2 = read_images(idx, GRAY)
-
-    # TODO: maybe change it to a different detector
     detector = cv2.AKAZE_create()
     kps1, des1 = detector.detectAndCompute(img1, None)
     kps2, des2 = detector.detectAndCompute(img2, None)
-
-    # show_key_points(idx, kps1, kps2)
-
+    show_key_points(idx, kps1, kps2)
     return des1, des2, img1, np.array(kps1), img2, np.array(kps2)
 
 
@@ -44,14 +38,14 @@ def match_key_points(des1, des2, img1, kps1, img2, kps2):
     res = np.empty(
         (max(img1.shape[0], img2.shape[0]), img1.shape[1] + img2.shape[1], 3),
         dtype=np.uint8)
-    # TODO: maybe change it to a different distance method
-    brute_force = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+    brute_force = cv2.BFMatcher(cv2.NORM_L1, crossCheck=True)
     matches = np.array(brute_force.match(des1, des2))
     random_matches = matches[np.random.randint(len(matches), size=20)]
     cv2.drawMatches(img1, kps1, img2, kps2, random_matches, res,
                     flags=cv2.DRAW_MATCHES_FLAGS_NOT_DRAW_SINGLE_POINTS)
-    cv2.imshow("Output matches", res)
+    cv2.imshow("Output matches", res)  # 1.3
     cv2.waitKey(0)
+
 
 
 def print_feature_descriptors(descriptors1, descriptors2):
@@ -65,32 +59,21 @@ def significance_test(des1, des2, img1, kps1, img2, kps2):
     res = np.empty(
         (max(img1.shape[0], img2.shape[0]), img1.shape[1] + img2.shape[1], 3),
         dtype=np.uint8)
-    # TODO: maybe change it to a different distance method
-    brute_force = cv2.BFMatcher(cv2.NORM_HAMMING)
+    brute_force = cv2.BFMatcher(cv2.NORM_L1)
     matches = brute_force.knnMatch(des1, des2, k=2)
-    # TODO: maybe change it to a different ratio value
-    ratio = 0.15
+    ratio = 0.5
     good_matches = np.array(
         [m1 for m1, m2 in matches if m1.distance < ratio * m2.distance])
     random_matches = good_matches[
         np.random.randint(len(good_matches), size=20)]
     cv2.drawMatches(img1, kps1, img2, kps2, random_matches, res,
-                    flags=cv2.DRAW_MATCHES_FLAGS_NOT_DRAW_SINGLE_POINTS
-                    )
-    cv2.imshow("Output good matches", res)
-
-    all_matches = np.array([m1 for m1, m2 in matches])[
-        np.random.randint(len(good_matches), size=10)]
-    cv2.drawMatches(img1, kps1, img2, kps2, all_matches, res,
-                    flags=cv2.DRAW_MATCHES_FLAGS_NOT_DRAW_SINGLE_POINTS
-                    )
-    cv2.imshow("Output all matches", res)
+                    flags=cv2.DRAW_MATCHES_FLAGS_NOT_DRAW_SINGLE_POINTS)
+    cv2.imshow("Output random good matches", res)  # 1.4
     cv2.waitKey(0)
 
 
 if __name__ == '__main__':
     des1, des2, img1, kps1, img2, kps2 = detect_key_points(0)
-
     print_feature_descriptors(des1, des2)
-
+    match_key_points(des1, des2, img1, kps1, img2, kps2)
     significance_test(des1, des2, img1, kps1, img2, kps2)
