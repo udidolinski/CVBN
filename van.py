5ed2334d3e65dbdb5ad64b6389cacee0ab69913d
udidolinski <udi672@gmail.com>
2022-04-07 23:13:01 +0300

    after 2.6 (needs more testing and 2.5)

--- van.py
+++ van.py
@@ -5,6 +5,8 @@ import matplotlib.pyplot as plt
 GRAY = 0
 RGB = 1
 DEVIATION_THRESHOLD = 2
+RANSAC_NUM_SAMPLES = 4
+RANSAC_SUCCESS_PROB = 0.9
 
 DATA_PATH = r'VAN_ex\dataset\sequences\00\\'
 
@@ -92,9 +94,9 @@ def histogram_pattern(matches, kps1, kps2):
         y1 = kps1[match.queryIdx].pt[1]
         y2 = kps2[match.trainIdx].pt[1]
         deviations[i] = abs(y2 - y1)
-    print("The percentage of matches that devaite by more than 2 pixel:",
-          round(100 * (len(deviations[deviations > DEVIATION_THRESHOLD]) / len(
-              deviations)), 2))
+    # print("The percentage of matches that devaite by more than 2 pixel:",
+    #       round(100 * (len(deviations[deviations > DEVIATION_THRESHOLD]) / len(
+    #           deviations)), 2))
     # plot_histogram(deviations)
     return deviations
 
@@ -175,7 +177,7 @@ def triangulate_all_points(matches, kps1, kps2, img1, img2):
         points[i] = our_p3d
 
     result_string = {False: "doesn't equal", True: "equals"}
-    print(f"Our triangulation {result_string[equal]} to cv triangulation")
+    # print(f"Our triangulation {result_string[equal]} to cv triangulation")
     return points.T
 
 
@@ -197,6 +199,12 @@ def plot_triangulations(x, y, z):
     ax.set_xlabel("x")
     ax.set_ylabel("y")
     ax.set_zlabel("z")
+    plt.show()
+
+def plot_trajectury(x, y):
+    plt.scatter(x, y)
+    plt.xlabel("x")
+    plt.ylabel("y")
     plt.show()
 
 
@@ -230,7 +238,7 @@ def rodriguez_to_mat(rvec, tvec):
     return np.hstack((rot, tvec))
 
 
-def pnp(img_idx1, img_idx2):
+def pnp(img_idx1, img_idx2, k, p3p=True, inliers_idx=None):
     matches_1, matches_2, left_left_matches, kps1_1, kps2_1, kps1_2, kps2_2, img1_1, img2_1, img1_2 = match_pair_images_points(img_idx1, img_idx2)
 
     matches_1_dict, matches_2_dict, left_left_matches_dict = index_dict_matches(matches_1),index_dict_matches(matches_2),index_dict_matches(left_left_matches)
@@ -241,14 +249,21 @@ def pnp(img_idx1, img_idx2):
             if (match1[0], match2[0]) in left_left_matches_dict:
                 kps1_2_matches_idx.append(match2[0])
                 good_matches1_idx.append(matches_1_dict[match1])
-    good_kps = kps1_2[kps1_2_matches_idx][:4]
+
+
+    indices = inliers_idx
+    flag = cv2.SOLVEPNP_EPNP
+    if p3p:
+        indices = np.random.choice(np.arange(len(kps1_2[kps1_2_matches_idx])), 4, replace=False)
+        flag = cv2.SOLVEPNP_AP3P
+
+
+    good_kps = kps1_2[kps1_2_matches_idx][indices]
     image_points = np.array([kps.pt for kps in good_kps])
 
-    points_3d = triangulate_all_points(matches_1[good_matches1_idx][:4], kps1_1, kps2_1, img1_1, img2_1).T
+    points_3d = triangulate_all_points(matches_1[good_matches1_idx][indices], kps1_1, kps2_1, img1_1, img2_1).T
 
-
-    k = read_cameras()[0]
-    _, rvec, tvec = cv2.solvePnP(points_3d, image_points, k, None, flags=cv2.SOLVEPNP_AP3P)
+    _, rvec, tvec = cv2.solvePnP(points_3d, image_points, k, None, flags=flag)
     R_t_1_2 = rodriguez_to_mat(rvec, tvec)
     left_1_location = transform_rt_to_location(R_t_1_2)
     return left_1_location, R_t_1_2
@@ -256,7 +271,7 @@ def pnp(img_idx1, img_idx2):
 def transform_rt_to_location(R_t):
     R = R_t[:, :3]
     t = R_t[:, 3]
-    return np.linalg.inv(R) @ (-t)
+    return R.T @ (-t)
 
 def compute_camera_locations(img_idx1, img_idx2):
     k, m1, m2 = read_cameras()
@@ -274,9 +289,9 @@ def compute_camera_locations(img_idx1, img_idx2):
     plot_triangulations(points[0], points[1], points[2])
 
 
-def pnp2(img_idx1, img_idx2):
+def find_inliers(img_idx1, img_idx2, R_t_1_2, k):
     matches_1, matches_2, left_left_matches, kps1_1, kps2_1, kps1_2, kps2_2, img1_1, img2_1, img1_2 = match_pair_images_points(img_idx1, img_idx2)
-    k = read_cameras()[0]
+    # k = read_cameras()[0]
     matches_1_dict, matches_2_dict, left_left_matches_dict = index_dict_matches(matches_1),index_dict_matches(matches_2),index_dict_matches(left_left_matches)
     matches_kps1_dict = {}
     good_matches1_idx = []
@@ -291,41 +306,115 @@ def pnp2(img_idx1, img_idx2):
     points_3d = triangulate_all_points(matches_1[good_matches1_idx], kps1_1, kps2_1, img1_1, img2_1)
     points_4d = np.vstack((points_3d, np.ones(points_3d.shape[1])))
 
-    R_t_1_2 = pnp(img_idx1, img_idx2)[1]
+    # R_t_1_2 = pnp(img_idx1, img_idx2)[1]
 
+    model_pixels_2d = perform_transformation_3d_points_to_pixels(R_t_1_2, k, points_4d)
+    real_pixels_2d = np.array([point.pt for point in kps1_2[kps1_2_matches_idx]]).T
+    diff_real_and_model = np.abs(real_pixels_2d - model_pixels_2d)
+    inliers_idx = np.where((diff_real_and_model[0] < 2) & (diff_real_and_model[1] < 2))[0]
+
+    # present_inliers_and_outliers(img_idx1, img_idx2, kps1_1,
+    #                              kps1_1_matches_idx, kps1_2,
+    #                              kps1_2_matches_idx, matches_kps1_dict,
+    #                              inliers_idx)
+
+    return len(inliers_idx), diff_real_and_model.shape[1] - len(inliers_idx), [img_idx1, img_idx2, kps1_1,
+                             kps1_1_matches_idx, kps1_2,
+                             kps1_2_matches_idx, matches_kps1_dict,
+                             inliers_idx]
+
+
+def perform_transformation_3d_points_to_pixels(R_t_1_2, k, points_4d):
     pixels_3d = k @ R_t_1_2 @ points_4d
     pixels_3d[0] /= pixels_3d[2]
     pixels_3d[1] /= pixels_3d[2]
-    pixels_2d = pixels_3d[:2]
-    real_pixels_2d = np.array([point.pt for point in kps1_2[kps1_2_matches_idx]]).T
+    model_pixels_2d = pixels_3d[:2]
+    return model_pixels_2d
 
-    result = np.abs(real_pixels_2d - pixels_2d)
-    result2 = np.where((result[0] < 2) & (result[1] < 2))[0]
 
+def present_inliers_and_outliers(img_idx1, img_idx2, kps1_1,
+                                 kps1_1_matches_idx, kps1_2,
+                                 kps1_2_matches_idx, matches_kps1_dict,
+                                 result2):
     best_matches_idxs = []
     best_kps1_1_idxs = []
     for idx in result2:
         kps1_1_idx, left_left_match_idx = matches_kps1_dict[kps1_2_matches_idx[idx]]
         best_matches_idxs.append(left_left_match_idx)
         best_kps1_1_idxs.append(kps1_1_idx)
-
-
     best_kps1_2_idxs = np.array(kps1_2_matches_idx)[result2]
-
     bad_keypoint1 = list(set(kps1_1_matches_idx) - set(best_kps1_1_idxs))
     bad_keypoint2 = list(set(kps1_2_matches_idx) - set(best_kps1_2_idxs))
-
     im1 = read_images(img_idx1, RGB)[0]
     im2 = read_images(img_idx2, RGB)[0]
-    draw_good_and_bad_matches(im1, kps1_1, best_kps1_1_idxs, bad_keypoint1, im2,
-                              kps1_2, best_kps1_2_idxs, bad_keypoint2, "left0", "left1")
+    draw_good_and_bad_matches(im1, kps1_1, best_kps1_1_idxs, bad_keypoint1,
+                              im2,
+                              kps1_2, best_kps1_2_idxs, bad_keypoint2, "left0",
+                              "left1")
 
-    s = 1
+def compute_num_of_iter(p, epsilon, s):
+    return np.log(1-p) / np.log(1 - ((1 - epsilon)**s))
+def ransac(img_idx1, img_idx2, k):
+    s = RANSAC_NUM_SAMPLES
+    p = RANSAC_SUCCESS_PROB
+    epsilon = 0.85
+
+    num_iter = compute_num_of_iter(p, epsilon, s)
+
+    max_num_inliers = 0
+    best_transformation = None
+    best_compute_lst = []
+    # Repeat 1
+    i = 0
+    while i <= num_iter:
+        current_transformation = pnp(img_idx1, img_idx2, k, True)[1]
+        current_num_inliers, current_num_outliers, current_compute_lst  = find_inliers(img_idx1, img_idx2, current_transformation, k)
+        if current_num_inliers > max_num_inliers:
+            best_transformation = current_transformation
+            best_compute_lst = current_compute_lst
+            max_num_inliers = current_num_inliers
+            new_epsilon = current_num_outliers / (current_num_inliers + current_num_outliers)
+            num_iter = compute_num_of_iter(p, new_epsilon, s)
+        i+=1
+    # Repeat 2
+    best_transformation2 = best_transformation
+    max_num_inliers2 = max_num_inliers
+    best_compute_lst2 = best_compute_lst
+
+    for j in range(3):
+        current_transformation = pnp(img_idx1, img_idx2, k, False, best_compute_lst[-1])[1]
+        if np.allclose(current_transformation, best_transformation2):
+            break
+        current_num_inliers, current_num_outliers, current_compute_lst = find_inliers(img_idx1, img_idx2,current_transformation, k)
+        if current_num_inliers > max_num_inliers:
+            best_transformation2 = current_transformation
+            max_num_inliers = current_num_inliers
+            best_compute_lst2 = current_compute_lst
+    present_inliers_and_outliers(*best_compute_lst2)
+    return best_transformation, best_compute_lst2
+
+def compute_extrinsic_matrix(transformation_0_to_i, transformation_i_to_i_plus_1):
+    R1 = transformation_0_to_i[:, :3]
+    t1 = transformation_0_to_i[:, 3]
+    R2 = transformation_i_to_i_plus_1[:, :3]
+    t2 = transformation_i_to_i_plus_1[:, 3]
+
+    new_R = R2 @ R1
+    new_t = R2 @ t1 + t2
+    return np.hstack((new_R, new_t[:,None]))
 
 
-def ransac(img_idx1, img_idx2):
-    pass
-
+def trajectory():
+    k = read_cameras()[0]
+    current_transformation = np.hstack((np.eye(3), np.zeros((3,1))))
+    locations = np.zeros((4, 3))
+    for i in range(3):
+        print(i)
+        transformation_i_to_i_plus_1 = ransac(i, i+1, k)[0]
+        transformation_0_to_i_plus_1 = compute_extrinsic_matrix(current_transformation, transformation_i_to_i_plus_1)
+        locations[i+1] = transform_rt_to_location(transformation_0_to_i_plus_1)
+        current_transformation = transformation_0_to_i_plus_1
+    return locations
 if __name__ == '__main__':
     # des1, des2, img1, kps1, img2, kps2 = detect_key_points(1)
     # # print_feature_descriptors(des1, des2)
@@ -338,5 +427,6 @@ if __name__ == '__main__':
     # points = (
     #     points.T[(np.abs(points[0]) < 25) & (np.abs(points[2]) < 100)]).T
     # plot_triangulations(points[0], points[1], points[2])
-
-    pnp2(0,1)
+    # print(trajectory())
+    locations = trajectory().T
+    plot_trajectury(locations[0], locations[2])
